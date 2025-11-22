1c181869afb0269aab03c74b60b45b73f00d4e0e
Santiago Bassani <santiagobassani2000@gmail.com>
2025-11-22 13:41:05 -0300

    2.9*: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const Filter = ({ filterValue, handleFilterChange }) => {
+  return(
+    <div>
+      <p>filter shown with  <input type="text" value={filterValue} onChange={handleFilterChange}/> </p>
+    </div>
+  )
+}
+
 const Names = ({persons}) => {
   return (
     <div>
@@ -13,12 +21,19 @@ const Names = ({persons}) => {
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 }
+  
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
-
+  
+  
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
 
   const addNewPerson = (event) => {
     event.preventDefault()
@@ -46,25 +61,34 @@ const App = () => {
     setNewNumber((event.target.value))
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  }
+
+  const personsToShow = persons.filter(person => 
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <Filter filterValue={filter} handleFilterChange={handleFilterChange}></Filter>
+      
+      <h2>add a new</h2>
+      <form>  
         <div>
           name: <input onChange={handleNameInputChange} value={newName} />
         </div>
         <div>
           number: <input onChange={handleNumberInputChange} value={newNumber} />
         </div>
-
         <div>
           <button type="submit" onClick={addNewPerson}>add</button>
         </div>
       </form>
 
       <h2>Numbers</h2>
-      
-      <Names persons={persons}></Names>
+      <Names persons={personsToShow}></Names>
       
     </div>
   )
