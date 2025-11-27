161d2c50dc54b15865f4012f6e536aefa63d13e4
Santiago Bassani <santiagobassani2000@gmail.com>
2025-11-27 00:01:42 -0300

    Note proyect code

--- follow-along-examples/notes/src/App.jsx
+++ follow-along-examples/notes/src/App.jsx
@@ -1,22 +1,26 @@
-import { useState } from "react"
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
 
-const Note = ({ note }) => {
-  return (
-    <li>{note.content}</li>
-  )
-}
-
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('') 
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/notes').then((response) => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+  }, [])
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5,
+      important: Math.random() > 0.5,
       id: String(notes.length + 1),
     }
 
@@ -24,15 +28,11 @@ const App = (props) => {
     setNewNote('')
   }
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important === true)
-
-
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewNote(event.target.value)
   }
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
   return (
     <div>
@@ -43,17 +43,16 @@ const App = (props) => {
         </button>
       </div>
       <ul>
-        {notesToShow.map(note =>
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
-        )}
+        ))}
       </ul>
       <form onSubmit={addNote}>
-
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
-      </form>   
+      </form>
     </div>
   )
 }
 
-export default App 
+export default App
