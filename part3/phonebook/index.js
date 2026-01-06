6d3fe41aa93c458c64459c9f5a41a4fcd978028e
Santiago Bassani <santiagobassani2000@gmail.com>
2026-01-06 14:42:03 -0300

    3.4: Phonebook backend step 4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-const data = [
+let data = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -57,6 +57,15 @@ app.get("/api/persons/:id", (request,response) => {
     } else {
         response.status(404).end();
     }
+})
+
+// ----------------------- DELETE ----------------------------------------
+
+app.delete("/api/persons/:id", (request,response) => {
+    const id = request.params.id;
+    data = data.filter((person) => person.id !== id);
+
+    response.status(204).end()
 })
 
 // ----------------------- PRINTS ----------------------------------------
