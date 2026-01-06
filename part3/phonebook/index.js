b58657ccddabbdf37541e0bb72f351776082169c
Santiago Bassani <santiagobassani2000@gmail.com>
2026-01-06 14:51:19 -0300

    3.6: Phonebook backend step 6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -65,8 +65,12 @@ app.post("/api/persons", (request, response) => {
 
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: "content missing",
+      error: "number or name missing",
     });
+  }
+
+  if (data.find((person) => person.name === body.name)) {
+    return response.status(400).json({ error: "name must be unique" });
   }
 
   const person = {
